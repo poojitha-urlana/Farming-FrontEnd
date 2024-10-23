d7b979084c820e6d648407cb32467f0f344d522f
Chandu Maharana <chandumaharana58537@gmail.com>
2024-10-23 15:23:36 +0530

    user mangement modify

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component ,ViewEncapsulation} from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../Services/user.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,RouterLink],
   templateUrl: './register-user.component.html',
-  styleUrls: ['./register-user.component.css']
+  styleUrls: ['./register-user.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterUserComponent {
   user = {
@@ -28,7 +30,7 @@ export class RegisterUserComponent {
     this.userService.registerUser(this.user).subscribe(
       (response) => {
         console.log('Registration successful:', response);
-        this.router.navigate(['/dash-board-user']);  
+        this.router.navigate(['/login-user']);  
       },
       (error) => {
         console.error('Registration failed:', error);
