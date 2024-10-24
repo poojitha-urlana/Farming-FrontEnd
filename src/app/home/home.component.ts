3053b28caf5ff4334f882a53a6eca9228fc21ed6
Poojitha <220101120194@cutm.ac.in>
2024-10-24 16:42:15 +0530

    change the features in homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { FooterComponent } from '../footer/footer.component';
+
 import { 
   faUser, 
   faHome, 
@@ -12,19 +15,17 @@ import {
   faCamera, 
   faSeedling, 
   faChartLine 
-} from '@fortawesome/free-solid-svg-icons';  // Importing all necessary icons
-import { FooterComponent } from '../footer/footer.component';
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule,FooterComponent],
+  imports: [CommonModule, FontAwesomeModule, FooterComponent, AnimateOnScrollModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   constructor(private library: FaIconLibrary) {
-    // Registering all icons used in the component
     library.addIcons(faUser, faHome, faCog, faTint, faThermometerHalf, faWater, faCamera, faSeedling, faChartLine);
   }
 }
